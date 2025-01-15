c47f67d6d56c90aca84f362da5655833586d91cd
Eyohen <henry.eyo2@gmail.com>
2025-01-15 12:37:03 +0100

    adjusting client for mobile

--- app/client/page.js
+++ app/client/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Client() {
     return (
-      <section className='p-16 bg-[#191970]'>
+      <section className='p-2 md:p-16 bg-[#191970]'>
         <div className='min-h-screen flex relative'>
       <div className="absolute inset-0 bg-cover bg-center bg-no-repeat" 
     style={{
